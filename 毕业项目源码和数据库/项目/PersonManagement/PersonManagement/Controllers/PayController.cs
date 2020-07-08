0c4b963be527fe69efbae2cea8971b0165f9831c
20001106 <15273572765@163.com>
2020-07-08 19:15:12 +0800

    完成

--- 毕业项目源码和数据库/项目/PersonManagement/PersonManagement/Controllers/PayController.cs
+++ 毕业项目源码和数据库/项目/PersonManagement/PersonManagement/Controllers/PayController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -41,21 +42,41 @@ namespace PersonManagement.Controllers
         //薪资发放
         public ActionResult PayGrant()
         {
+            ViewBag.Person =  db.Person.ToList();
+            var pay = db.Pay.Where(p => p.OverTime.Year.ToString() == Date.Now.Year.ToString() && p.OverTime.Month.ToString() == Date.Now.Month.ToString() && p.OverTime.Day.ToString() == Date.Now.Day.ToString()).ToList();
+            if (pay.Count() == 0)
+            {
+                pay = null;
+            }
+            ViewBag.Pay = pay;
             return View();
         }
+        //下发
+        public ActionResult PaySubmit(int[] PersonID, int[] AttPay, int[] OtherPay)
+        {
+            for (int i = 0; i < PersonID.Length; i++)
+            {
+                Pay pay = new Pay();
+                pay.PersonID = PersonID[i];
+                pay.OverTime = Date.Now;
+                pay.AttPay = AttPay[i];
+                pay.OtherPay = OtherPay[i];
+                db.Pay.Add(pay);
+            }
+            db.SaveChanges();
+            return RedirectToAction("PayInfo","Pay");
+        }
 
-        //统计
+        //统计图
         public ActionResult Statistics()
         {
+            ViewBag.Person1 = db.Person.Where(p => p.Name == "张三").SingleOrDefault();
+            ViewBag.Person2 = db.Person.Where(p => p.Name == "李琼").SingleOrDefault();
+            ViewBag.Person3 = db.Person.Where(p => p.Name == "王五").SingleOrDefault();
+            ViewBag.Person4 = db.Person.Where(p => p.Name == "赵茜").SingleOrDefault();
+            ViewBag.Person5 = db.Person.Where(p => p.Name == "陈七").SingleOrDefault();
+            ViewBag.Person6 = db.Person.Where(p => p.Name == "王七").SingleOrDefault();
             return View();
-        }
-
-        [HttpGet]
-        //数据
-        public JsonResult PayData()
-        {
-            
-            return Json(new {  }, JsonRequestBehavior.AllowGet);
         }
     }
 }
